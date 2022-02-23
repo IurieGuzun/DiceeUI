c3d2986ace5c3b19d308b30007530437757900a8
Iurie Guzun <iurie_guzun@yahoo.com>
2022-02-23 11:54:45 -0500

    Button Added

--- DiceeUI/ContentView.swift
+++ DiceeUI/ContentView.swift
@@ -18,7 +18,15 @@ struct ContentView: View {
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(width: 100, height: 100, alignment: .center)
-                DiceView()
+                HStack {
+                    DiceView(n: 2)
+                    DiceView(n: 4)
+                }
+                .padding()
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+                }
+                .padding()
             }
             
             
@@ -29,8 +37,11 @@ struct ContentView: View {
 }
 
 struct DiceView: View {
+    
+    let n: Int
+    
     var body: some View {
-        Image("dice")
+        Image("dice\(n)")
             .resizable()
             .aspectRatio(1, contentMode: .fit)
     }
